0f8c39d98551632060fa38e9eba1d155a54583cd
Andrii Panasiuk <andrii.panasiuk@ringteam.com>
2022-06-14 12:17:43 +0300

    Fixed build, possibly?

--- srcs/2022-06-14.nix
+++ srcs/2022-06-14.nix
@@ -1,18 +1,22 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "nixpkgs-22.11pre385130.3d2819ec0b9";
+  name = "nixpkgs-22.11pre385191.3c2068d6c90";
   version = "2022-06-13";
 
   src = fetchurl {
     url = "https://releases.nixos.org/nixpkgs/${name}/nixexprs.tar.xz";
-    sha256 = "83f5b46ebfba5f978794158df9cb3515128d6b39ab597d2b9495d4809b3f9275";
+    sha256 = "9fd6032013fe75b351e680621d493b12d1e8b48fe4008b9ae6e0408296ce6af8";
   };
 
+  # buildInputs = [ findutils ];
+
+  dontUnpack = true;
   dontBuild = true;
   preferLocalBuild = true;
 
   installPhase = ''
+    find . -executable -type f | xargs -n1 sed -i"" -e 's/bin\/env -S/bin\/env/g'
     cp -a . $out
   '';
 }
